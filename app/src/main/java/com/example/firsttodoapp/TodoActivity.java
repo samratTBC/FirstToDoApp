eab1a82ec45d2c98db7f2fd46c705b1a6580104d
samratTBC <samratjung12@gmail.com>
2023-04-17 16:21:50 +0545

    Initial logic created for FirstToDoApp, created layout, logic, arrays and listeners.

---
+++ app/src/main/java/com/example/firsttodoapp/TodoActivity.java
@@ -0,0 +1,48 @@
+package com.example.firsttodoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TodoActivity extends AppCompatActivity {
+
+
+    private TextView todoTextView;
+
+    private String [] mTodos;
+
+    private Button buttonNext;
+    private Button buttonPrev;
+
+    private int mTodoIndex;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+
+        todoTextView =(TextView) findViewById(R.id.textViewTodo);
+        buttonNext = (Button) findViewById(R.id.buttonNext);
+        buttonPrev = (Button) findViewById(R.id.buttonPrev);
+
+        mTodos = getResources().getStringArray(R.array.todos);
+        mTodoIndex =0;
+
+        todoTextView.setText(mTodos[mTodoIndex]);
+
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTodoIndex++;
+                todoTextView.setText(mTodos[mTodoIndex]);
+            }
+        });
+
+    }
+}
